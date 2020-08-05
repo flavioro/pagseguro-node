4dcec38d197d93c2d1221ba17b76150a1298ef77
Flavio Rodrigues <flavioro@gmail.com>
2020-08-05 09:18:54 -0300

    Adicionado arquivos config.js para o git, comentado em .gitignore

---
+++ example/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  email : "financeiro@archshop.com.br",
+  token: "914C9DB3D10F4395B55EDDE7FECA3A79",
+  appId: "app9513284614",
+  appKey: "3514BF1BEFEFA31334C6DFA55BDAF9D0",
+
+  env: "sandbox",
+  log: __dirname + "/log/pagseguro.log",
+  debug: false,
+  notificationURL: "http://localhost:3333/authorization/notification",
+  redirectURL: "http://localhost:3333/authorization/response"
+};
